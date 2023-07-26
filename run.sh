f49142af14f638983d9c87ca2f9a21752084433f
Lexra <jasonc@mail2000.com.tw>
2023-07-26 15:02:27 +0800

    python3 coco-manager/filter.py --input_json annotations/instances_train2017.json --output_json annotations/filter_train2017.json --categories 'person'

--- run.sh
+++ run.sh
@@ -35,10 +35,11 @@ if [ ! -e train2017/000000581929.jpg ]; then
 	rm -rf val2017 train2017 annotations
 	unzip -o train2017.zip
 	unzip -o val2017.zip
-	unzip -o annotations_trainval2017.zip
 	#rm -rf _train2017 ; mkdir -p _train2017; for J in `ls train2017`; do python3 change24.py $(pwd)/train2017/$J $(pwd)/_train2017/$J ; mv -fv $(pwd)/_train2017/$J $(pwd)/train2017/$J ; done
 	#rm -rf _val2017 ; mkdir -p _val2017; for J in `ls val2017`; do python3 change24.py $(pwd)/val2017/$J $(pwd)/_val2017/$J ; mv -fv $(pwd)/_val2017/$J $(pwd)/val2017/$J ; done
 fi
+rm -rfv annotations
+unzip -o annotations_trainval2017.zip
 
 ##############################
 git clone https://github.com/tw-yshuang/coco2yolo.git || true
@@ -49,6 +50,12 @@ export PYTHONPATH=`pwd`/coco2yolo:${PYTHONPATH}
 echo ""
 echo -e "${YELLOW} => Generate annotation txt files for train2017 ${NC}"
 find train2017 -name "*.txt" | xargs rm -rf
+if [ "${NAME}" == "yolo-person" ]; then
+	python3 coco-manager/filter.py --input_json annotations/instances_train2017.json --output_json annotations/filter_train2017.json --categories 'person'
+	mv -fv annotations/filter_train2017.json annotations/instances_train2017.json
+	python3 coco-manager/filter.py --input_json annotations/instances_val2017.json --output_json annotations/filter_val2017.json --categories 'person'
+	mv -fv annotations/filter_val2017.json annotations/instances_val2017.json
+fi
 python3 COCO2YOLO/COCO2YOLO.py -j annotations/instances_train2017.json -o train2017
 
 echo -e "${YELLOW} => Generate annotation txt files for val2017 ${NC}"
@@ -57,11 +64,11 @@ python3 COCO2YOLO/COCO2YOLO.py -j annotations/instances_val2017.json -o val2017
 
 ##############################
 echo -e "${YELLOW} => Generate train.txt for all train2017 annotations ${NC}"
-for J in $(ls train2017 | grep txt | awk -F '.' '{print $1}'); do echo "$(pwd)/train2017/${J}.jpg" ; done | tee train.txt
+for J in $(ls train2017 | grep txt | awk -F '.txt' '{print $1}'); do echo "$(pwd)/train2017/${J}.jpg" ; done | tee train.txt
 
 ##############################
 echo -e "${YELLOW} => Generate valid.txt for all val2017 annotations ${NC}"
-for J in $(ls val2017 | grep txt | awk -F '.' '{print $1}'); do echo "$(pwd)/val2017/${J}.jpg" ; done | tee valid.txt
+for J in $(ls val2017 | grep txt | awk -F '.txt' '{print $1}'); do echo "$(pwd)/val2017/${J}.jpg" ; done | tee valid.txt
 
 ##############################
 sed "s|/work/himax/Yolo-Fastest/COCO-2017|`pwd`|" -i cfg/${NAME}.data
@@ -72,7 +79,7 @@ echo -e "${YELLOW} => Detector train ${NC}"
 [ -e ../data/labels/100_0.png ] && ln -sf ../data .
 mkdir -p backup
 [ -e backup/${NAME}_last.weights ] && WEIGHTS=backup/${NAME}_last.weights
-../darknet detector train cfg/${NAME}.data ${CFG} ${WEIGHTS} ${GPUS} -mjpeg_port 8090
+../darknet detector train cfg/${NAME}.data ${CFG} ${WEIGHTS} ${GPUS} -mjpeg_port 8090 -map
 
 ##############################
 if [ -e ../keras-YOLOv3-model-set/tools/model_converter/fastest_1.1_160/convert.py ]; then
@@ -86,17 +93,21 @@ if [ -e ../keras-YOLOv3-model-set/tools/model_converter/fastest_1.1_160/convert.
 	#python3 ../keras-YOLOv3-model-set/tools/model_converter/keras_to_tensorflow.py --input_model backup/${NAME}.h5 --output_model backup/${NAME}.tflite
 	python3 ../keras-YOLOv3-model-set/tools/model_converter/fastest_1.1_160/post_train_quant_convert_demo.py --keras_model_file backup/${NAME}.h5 --annotation_file train.txt --output_file backup/${NAME}.tflite
 
+	echo -e "${YELLOW} => Generate backup/${NAME}-$(date +'%Y%m%d').cc file ${NC}"
+	xxd -i backup/${NAME}.tflite > backup/${NAME}-$(date +'%Y%m%d').cc
+	ls -l backup/${NAME}-$(date +'%Y%m%d').cc
+	echo ""
+	cat backup/${NAME}-$(date +'%Y%m%d').cc | grep tflite_len
+
 	#python3 ../keras-YOLOv3-model-set/eval_yolo_fastest_160_1ch_tflite.py \
 	#       --model_path backup/${NAME}.tflite --anchors_path cfg/${NAME}.anchors --classes_path cfg/${NAME}.names --annotation_file train.txt --json_name ${NAME}.json || true
 	#python3 ../pycooc_person.py \
 	#       --res_path ../keras-YOLOv3-model-set/coco_results/${NAME}.json --instances_json_file annotations/instances_train2017.json || true
-
-	echo -e "${YELLOW} => Generate the ${NAME}.cc file ${NC}"
-	xxd -i backup/${NAME}.tflite > backup/${NAME}.cc
 fi
 
 ##############################
 echo ""
 echo -e "${YELLOW} Detector Test: ${NC}"
 echo -e "${YELLOW} ../darknet detector test cfg/${NAME}.data cfg/${NAME}.cfg backup/${NAME}_final.weights COCO-2017/pixmaps/people.jpg ${NC}"
+echo ""
 exit 0
